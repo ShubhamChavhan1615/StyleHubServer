0650da8ea1eb94baeb37f224c7e8dd3498e19d63
Shubham_Chavhan <146634527+ShubhamChavhan1615@users.noreply.github.com>
2024-06-17 15:43:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import "dotenv/config";
+import indexRouter from "./routes/index.js";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import db from "./db/db.js";
+
+const app = express();
+const port = process.env.PORT
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//routes
+app.use("/", indexRouter);
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+})
