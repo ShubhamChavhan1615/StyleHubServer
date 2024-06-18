2a80848259cabdadc9a93b3c12196d2ee8d854d7
Shubham_Chavhan <146634527+ShubhamChavhan1615@users.noreply.github.com>
2024-06-18 09:25:39 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const localDb = process.env.LOCAL_DB;
 
-const db = await mongoose.connect("mongodb+srv://shubhamchav1615:otcZQp2wbkKt5p6z@cluster0.n4u8tue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+const db = await mongoose.connect("mongodb+srv://shubhamchav1615:otcZQp2wbkKt5p6z@cluster0.n4u8tue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Demo")
     .then(() => {
-        console.log("db connected");
+        console.log("connected to db");
     })
     .catch(err => {
         console.log(err);
